07addcb242f2d6c5abd3f65eae582ce0ff24d5bb
Blak3Nick <blaken2021@gmail.com>
2018-11-12 14:49:07 -0500

    Created a new test db

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:D:\\databases\\testjava.db");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:E:\\sqllite\\databases\\testjava.db");
         } catch (SQLException e) {
-            System.out.println("something went wrong" + e.getMessage());
+            System.out.println("something went wrong " + e.getMessage());
         }
     }
 }
