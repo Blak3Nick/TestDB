8003669cfda74a129a48d7250fc7ce76f6f6ff12
Blak3Nick <blaken2021@gmail.com>
2018-11-12 15:25:36 -0500

    adding new tables

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:E:\\sqllite\\databases\\testjava.db");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:testjava.db");
+            Statement statement = conn.createStatement();
+            statement.execute("CREATE TABLE contacts (name TEXT, phone INTEGER, email TEXT)");
+            statement.close();
+            conn.close();
         } catch (SQLException e) {
             System.out.println("something went wrong " + e.getMessage());
         }
