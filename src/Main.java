a90ed5aed7ac463633b6509c0cff2064cf953070
Blak3Nick <blaken2021@gmail.com>
2018-11-13 14:08:22 -0500

    working with sql statements

--- src/Main.java
+++ src/Main.java
@@ -16,17 +16,34 @@ public class Main {
             Connection conn = DriverManager.getConnection(CONNECTION_STRING);
             Statement statement = conn.createStatement();
             statement.execute("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
-            statement.execute("CREATE TABLE IF NOT EXISTS" + TABLE_CONTACTS +
+            statement.execute("CREATE TABLE IF NOT EXISTS " + TABLE_CONTACTS +
                                     " (" + COLUMN_NAME + " text, " +
-                                            COLUMN_PHONE + "integer, " +
+                                            COLUMN_PHONE + " integer, " +
                                             COLUMN_EMAIL + " text" +
-                                    "(");
+                                    ")");
             statement.execute("INSERT INTO " + TABLE_CONTACTS +
                                 "(" + COLUMN_NAME + ", " +
                                         COLUMN_PHONE + ", " +
                                         COLUMN_EMAIL +
                                 ")" +
                                 "VALUES('Tim', 6456458, 'tim@email.com')");
+            statement.execute("INSERT INTO " + TABLE_CONTACTS +
+                    "(" + COLUMN_NAME + ", " +
+                    COLUMN_PHONE + ", " +
+                    COLUMN_EMAIL +
+                    ")" +
+                    "VALUES('Bill', 1234567, 'bill@email.com')");
+            statement.execute("INSERT INTO " + TABLE_CONTACTS +
+                    "(" + COLUMN_NAME + ", " +
+                    COLUMN_PHONE + ", " +
+                    COLUMN_EMAIL +
+                    ")" +
+                    "VALUES('Jill', 4568797, 'jill@email.com')");
+            statement.execute("UPDATE " + TABLE_CONTACTS + " SET " +
+                    COLUMN_PHONE + "=77777777" +
+                    " WHERE " + COLUMN_NAME + "='Tim'");
+            statement.execute("DELETE FROM " + TABLE_CONTACTS +
+                    " WHERE " + COLUMN_NAME + "='Tim'");
 
 //            statement.execute("INSERT INTO contacts (name, phone, email)" +
 //                    "VALUES ('Blake', 6452323, 'email.com' )");
@@ -39,11 +56,11 @@ public class Main {
 
 //            statement.execute("SELECT * FROM contacts");
 //            ResultSet results = statement.getResultSet();
-            ResultSet results = statement.executeQuery("SELECT * FROM contacts");
+            ResultSet results = statement.executeQuery("SELECT * FROM " + TABLE_CONTACTS);
             while (results.next()) {
-                System.out.println( results.getString("name") + " "+
-                                    results.getInt("phone") + " " +
-                                    results.getString("email"));
+                System.out.println( results.getString(COLUMN_NAME) + " "+
+                                    results.getInt(COLUMN_PHONE) + " " +
+                                    results.getString(COLUMN_EMAIL));
             }
             results.close();
 
@@ -51,6 +68,7 @@ public class Main {
             conn.close();
         } catch (SQLException e) {
             System.out.println("something went wrong " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
